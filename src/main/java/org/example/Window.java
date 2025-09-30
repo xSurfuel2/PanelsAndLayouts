051d924e561d64d7351270966f0114e49e7eac97
xSurfuel2 <xSurfuel2@gmail.com>
2025-09-30 12:33:53 +0200

    v0.1

---
+++ src/main/java/org/example/Window.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Window {
+    private JPanel JPanel1;
+    private JPanel headerPanel;
+    private JPanel navPanel;
+    private JPanel formPanel;
+    private JPanel previewPanel;
+    private JTabbedPane tabbedPane1;
+    private JPanel Dashboard;
+    private JPanel Usuarios;
+    private JPanel Informes;
+    private JPanel Ajustes;
+    private JPanel Ayuda;
+    private JPanel buttonBar;
+}
